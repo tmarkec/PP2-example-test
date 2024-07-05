05a5e215c0897a497e39e1dd6af12df4f5673ab2
tmarkec <105808713+tmarkec@users.noreply.github.com>
2024-07-05 09:06:27 +0000

    comment out second function

--- assets/js/script.js
+++ assets/js/script.js
@@ -17,22 +17,25 @@ function sortFruit() {
 }
 
 sortFruit()
-const fruits = ["🍌", "🍏", "🍊", "🍌", "🍌", "🍏", "🍊", "🍏", "🍊", "🍊", "🍏", "🍌"];
 
-let bananaShelf = document.getElementById("banana-shelf");
-let orangeShelf = document.getElementById("orange-shelf");
-let appleShelf = document.getElementById("apple-shelf");
 
-function sortFruit() {
-    for (let i = 0; i < fruits.length; i++) {
-        if (fruits[i] === "🍌") {
-            bananaShelf.innerHTML += ` ${fruits[i]}`;
-        } else if (fruits[i] === "🍊") {
-            orangeShelf.innerHTML += ` ${fruits[i]}`;
-        } else {
-            appleShelf.innerHTML += ` ${fruits[i]}`;
-        }
-    }
-}
+//similar approach to the problem, have a  look at the loops in both examples which approach your prefer and understand more
+// const fruits = ["🍌", "🍏", "🍊", "🍌", "🍌", "🍏", "🍊", "🍏", "🍊", "🍊", "🍏", "🍌"];
+
+// let bananaShelf = document.getElementById("banana-shelf");
+// let orangeShelf = document.getElementById("orange-shelf");
+// let appleShelf = document.getElementById("apple-shelf");
+
+// function sortFruit() {
+//     for (let i = 0; i < fruits.length; i++) {
+//         if (fruits[i] === "🍌") {
+//             bananaShelf.innerHTML += ` ${fruits[i]}`;
+//         } else if (fruits[i] === "🍊") {
+//             orangeShelf.innerHTML += ` ${fruits[i]}`;
+//         } else {
+//             appleShelf.innerHTML += ` ${fruits[i]}`;
+//         }
+//     }
+// }
 
 
