cacdc81e2b4e9e7a6aca190534cce7772375b1e7
tmarkec <105808713+tmarkec@users.noreply.github.com>
2024-07-05 08:45:51 +0000

    add function to sort fruit, different approach

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,38 @@
-fruits = ["🍌", "🍏", "🍊", "🍌", "🍌", "🍏", "🍊", "🍏", "🍊", "🍊", "🍏", "🍌"];
+const fruits = ["🍌", "🍏", "🍊", "🍌", "🍌", "🍏", "🍊", "🍏", "🍊", "🍊", "🍏", "🍌"];
+
+let bnana = document.getElementById("banana-shelf");
+let orange = document.getElementById("orange-shelf");
+let apple = document.getElementById("apple-shelf");
+
+function sortFruit() {
+    for (let fruit of fruits) {
+        if (fruit === "🍌") {
+            bnana.innerHTML += ` ${fruit}`;
+        } else if (fruit === "🍊") {
+            orange.innerHTML += ` ${fruit}`;
+        } else {
+            apple.innerHTML += ` ${fruit}`;
+        }
+    }
+}
+
+sortFruit()
+const fruits = ["🍌", "🍏", "🍊", "🍌", "🍌", "🍏", "🍊", "🍏", "🍊", "🍊", "🍏", "🍌"];
+
+let bananaShelf = document.getElementById("banana-shelf");
+let orangeShelf = document.getElementById("orange-shelf");
+let appleShelf = document.getElementById("apple-shelf");
+
+function sortFruit() {
+    for (let i = 0; i < fruits.length; i++) {
+        if (fruits[i] === "🍌") {
+            bananaShelf.innerHTML += ` ${fruits[i]}`;
+        } else if (fruits[i] === "🍊") {
+            orangeShelf.innerHTML += ` ${fruits[i]}`;
+        } else {
+            appleShelf.innerHTML += ` ${fruits[i]}`;
+        }
+    }
+}
+
+
